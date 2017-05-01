945cf4241c86bf0d728c51d28facb718ec4d4865
collins lidede <lidedec@hotmail.com>
2017-05-01 17:15:42 -0400

    Update smartmirror.py

--- smartmirror.py
+++ smartmirror.py
@@ -287,7 +287,7 @@ class Calendar(Frame):
         feed = feedparser.parse(calendar_feed)
         for post in feed.entries[0:5]:
                calendar_event = CalendarEvent(self.calendarEventContainer)
-            calendar_event.pack(side=TOP, anchor=E)
+                calendar_event.pack(side=TOP, anchor=E)
     except Exception as e:
             traceback.print_exc()
             print "Error: %s. Cannot get calendar." % e
